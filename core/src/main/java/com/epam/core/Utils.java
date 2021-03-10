cb9d3576f9990b3234df4e60cbe11bd23e3cf292
”Alexander <alexander.kitanin@gmail.com>
2021-03-10 19:07:22 +0300

    Gradle build configured, resolving dependencies from custom repository configured, core module added with Utils class, api module added with App class

---
+++ core/src/main/java/com/epam/core/Utils.java
@@ -0,0 +1,16 @@
+package com.epam.core;
+
+import com.epam.utils.StringUtils;
+
+public class Utils {
+
+    public boolean isAllPositiveNumbers(String... str) {
+        StringUtils stringUtils = new StringUtils();
+        for (String number : str) {
+            if (!stringUtils.isPositiveNumber(number)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
